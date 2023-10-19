0d03a62ec7d48cda8a22c26702abb08aa3296bba
isacastro551 <100885098+isacastro551@users.noreply.github.com>
2023-10-19 09:50:02 -0300

    Update tela_produtos.dart

--- telas/tela_produtos.dart
+++ telas/tela_produtos.dart
@@ -1,17 +1,49 @@
-import 'package:bela/models/categoria.dart';
+import 'package:bela/models/produtos.dart';
 import 'package:flutter/material.dart';
+import '../componentes/produto_item.dart';
 
-class TelaProdutos extends StatelessWidget {
+class TelaProdutos extends StatefulWidget {
+  static const routeName = '/produtos';
+  final List<Produto> produtosValidos;
+  TelaProdutos(this.produtosValidos);
 
+  @override
+  State<TelaProdutos> createState() => _TelaProdutosState();
+}
 
-      @override
-      Widget build(BuildContext context) {
-        final categoria = ModalRoute.of(context)?.settings.arguments as Categoria;
-        return Scaffold(
-            appBar: AppBar (
-                title: Text("Produtos de Categoria"),
-            ),
-            body: Text("Produtos de Categoria ${categoria.titulo}"),
-        );
-      }
+class _TelaProdutosState extends State<TelaProdutos> {
+  String? tituloCategoria;
+  List<Produto>? displayProdutos;
+
+  @override
+  void didChangeDependencies() {
+   
+    final routeArgs =
+        ModalRoute.of(context)!.settings.arguments as Map<String, String>;
+    tituloCategoria = routeArgs['title']!;
+    final categoryId = routeArgs['id'];
+    displayProdutos = widget.produtosValidos.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(tituloCategoria!)),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return ItemProduto(
+            id: displayProdutos![index].id,
+            title: displayProdutos![index].title,
+            imageUrl: displayProdutos![index].imageUrl,
+            duration: displayProdutos![index].duration,
+            cost: displayProdutos![index].cost,
+          );
+        },
+        itemCount: displayProdutos!.length,
+      ),
+    );
+  }
 }
